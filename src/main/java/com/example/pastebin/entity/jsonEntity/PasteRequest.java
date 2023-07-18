4083b1789edc8db63731410af967528f33dfa41d
KseniaLune <ksenia00nuysha@gmail.com>
2023-07-18 17:56:59 +0300

    fix some problem

--- src/main/java/com/example/pastebin/entity/jsonEntity/PasteRequest.java
+++ src/main/java/com/example/pastebin/entity/jsonEntity/PasteRequest.java
@@ -1,14 +1,21 @@
 package com.example.pastebin.entity.jsonEntity;
 
 import com.example.pastebin.entity.Paste;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
+@Builder
 public class PasteRequest {
 
     private String data;
+    @Builder.Default
+    @JsonProperty("status")
     private Paste.AccessStatus accessStatus = Paste.AccessStatus.PUBLIC;
+    @Builder.Default
+    @JsonProperty("lifetime")
     private Long expirationTimeMinutes = 1440L;
+
+
 }
